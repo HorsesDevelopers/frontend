096f4a2065191dedab4726a0b5311d5724b9a9b8
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2025-05-13 21:58:08 -0500

    feat: iam base configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {authenticationGuard} from "./iam/services/authentication.guard";
+import {SignInComponent} from "./iam/pages/sign-in/sign-in.component";
+import {SignUpComponent} from "./iam/pages/sign-up/sign-up.component";
+export const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent},
+  ];
