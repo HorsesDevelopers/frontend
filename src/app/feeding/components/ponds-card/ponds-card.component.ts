22d86bd0aee4894674fe06ba02699aaf409ad073
Gabotw <u202018627@upc.edu.pe>
2025-05-14 00:31:54 -0500

    feat: added ponds-card component.

---
+++ src/app/feeding/components/ponds-card/ponds-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ponds-card',
+  imports: [],
+  templateUrl: './ponds-card.component.html',
+  styleUrl: './ponds-card.component.css'
+})
+export class PondsCardComponent {
+
+}
