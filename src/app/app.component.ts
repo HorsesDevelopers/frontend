1c04949f313f3e96a9a489f024ff22e860e3d987
Gabotw <u202018627@upc.edu.pe>
2025-05-14 00:30:55 -0500

    feat: added default configuration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {PondsViewComponent} from './feeding/pages/ponds-view/ponds-view.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PondsViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
