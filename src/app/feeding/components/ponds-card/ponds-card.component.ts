9924fe50b2310c32d5f035c140a208356213c8ba
Gabotw <u202018627@upc.edu.pe>
2025-05-14 00:42:53 -0500

    feat: update ponds-card component.

--- src/app/feeding/components/ponds-card/ponds-card.component.ts
+++ src/app/feeding/components/ponds-card/ponds-card.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {MatCard, MatCardContent} from '@angular/material/card';
 
 @Component({
   selector: 'app-ponds-card',
-  imports: [],
+  imports: [
+    MatCardContent,
+    MatCard
+  ],
   templateUrl: './ponds-card.component.html',
   styleUrl: './ponds-card.component.css'
 })
 export class PondsCardComponent {
-
+  @Input() pondName: string = '';
+  @Input() sensorCount: number = 0;
+  @Input() fishCount: number = 0;
+  @Input() imageUrl: string = 'assets/default-pond.jpg'; // Default image path
 }
--- src/app/feeding/pages/ponds-view/ponds-view.component.ts
+++ src/app/feeding/pages/ponds-view/ponds-view.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatButton} from '@angular/material/button';
+import {PondsCardComponent} from "../../components/ponds-card/ponds-card.component";
 
 @Component({
   selector: 'app-ponds-view',
-  imports: [
-    MatButton
-  ],
+    imports: [
+        MatButton,
+        PondsCardComponent
+    ],
   templateUrl: './ponds-view.component.html',
   styleUrl: './ponds-view.component.css'
 })
