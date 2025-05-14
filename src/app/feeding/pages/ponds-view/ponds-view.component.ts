da2b09837db57c15ab01a3db6cb5c2026510ccb2
Gabotw <u202018627@upc.edu.pe>
2025-05-14 00:32:02 -0500

    feat: added ponds-view component.

---
+++ src/app/feeding/pages/ponds-view/ponds-view.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-ponds-view',
+  imports: [
+    MatButton
+  ],
+  templateUrl: './ponds-view.component.html',
+  styleUrl: './ponds-view.component.css'
+})
+export class PondsViewComponent {
+
+}
